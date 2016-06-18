066a27b230911e26688034d09c356efedb75cc35
Horosphere <henrywen99@gmail.com>
2016-06-17 22:55:06 -0700

    Switch from qmake to cmake for easier library linking.

--- update.py
+++ update.py
@@ -1,43 +1,31 @@
 # Author: Horosphere
-#
-# Execute this script each time you introduce a new source or Qt moc header
-# file to the src/ directory. Currently do not support form files.
-#
-# Feed the script with the .pro file name if you have multiple .pro files.
-#
-# We don't know what would happen if there are no header files/source files
+# Execute this script each time you add a new source file in src/ to
+# update CMakeLists.txt
 
 import os, sys
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)));
-source_directory = "src"
+
+# dir_qObject must reside in dir_source
+dir_qObject = "src/ui"
+dir_source = "src"
+fileName = "CMakeLists.txt"
+
 signature_begin = "# Auto-generated. Do not edit. All changes will be undone\n"
 signature_end = "# Auto-generated end\n"
+autoGen_qObject_begin = "set(QObjectHeaders\n"
+autoGen_source_begin = "set(SourceFiles\n"
+autoGen_padding = "    "
+autoGen_end = "   )\n"
 
-# Retroactively line continued to prevent \ from appearing on the last line
-headers_begin = "HEADERS += "
-sources_begin = "\nSOURCES += "
-autoGen_padding = "\\\n\t" + source_directory
 
-fileName = ""
-if len(sys.argv) >= 2:
-    fileName = sys.argv[1]
-else:
-    for file in os.listdir(os.getcwd()):
-        if file.endswith(".pro"):
-            fileName = str(file)
-        else:
-            continue
-
-print("qmake project: " + fileName)
-
-projectFile = [] # Lines of project file
+cMakeFile = [] # Lines of CMakeLists.txt
 iBegin = -1 # Indices corresponding to auto-generated sections
 iEnd = -1
 with open(fileName, "r") as file:
     index = 0
     for line in file:
-        projectFile.append(line)
+        cMakeFile.append(line)
         if line == signature_begin:
             iBegin = index
         elif line == signature_end:
@@ -51,29 +39,33 @@ if iBegin == -1 or iEnd == -1 or iBegin > iEnd:
     print(signature_end[:-1])
     sys.exit(1)
 
-del projectFile[iBegin + 1: iEnd]
+del cMakeFile[iBegin + 1: iEnd]
 
-autoGenHeaders = headers_begin
-autoGenSources = sources_begin
+autoGen = autoGen_source_begin
+truncation = len("./" + dir_source)
 # Scan src/ directory recursively to gather source files
-for root, subdirs, files in os.walk("./" + source_directory): 
+for root, subdirs, files in os.walk("./" + dir_source): # The root string has length 5
     for name in files:
-        # Truncated filename. This is a legacy from CMakeLists generator
-        f = str(os.path.join(root, name))[len(source_directory) + 2:]
-        if f.endswith(('.h', '.hh', '.hpp')):
-            print("Header: " + f)
-            autoGenHeaders += autoGen_padding + f
-        elif f.endswith(('c', '.cc', '.cpp')):
-            print("Source: " + f)
-            autoGenSources += autoGen_padding + f
-        else:
-            continue
+        f = str(os.path.join(root, name))[truncation:]
+        if not f.endswith(('.cpp', '.c', '.cxx', '.cc')): continue
+        print("Source: " + f)
+        autoGen += autoGen_padding + dir_source + f + "\n"
+autoGen += autoGen_end + autoGen_qObject_begin
 
-autoGen = autoGenHeaders + autoGenSources + "\n"
-projectFile.insert(iBegin + 1, autoGen)
+truncation = len("./" + dir_qObject)
+# Scan src/ directory recursively to gather source files
+for root, subdirs, files in os.walk("./" + dir_qObject): # The root string has length 5
+    for name in files:
+        f = str(os.path.join(root, name))[truncation:]
+        if not f.endswith(('.hpp', '.h', '.hxx', '.hh')): continue
+        print("QObject: " + f)
+        autoGen += autoGen_padding + dir_qObject + f + "\n"
+autoGen += autoGen_end
+
+cMakeFile.insert(iBegin + 1, autoGen)
 
 with open(fileName, "w") as file:
-    for line in projectFile:
+    for line in cMakeFile:
         file.write(line)
 
 
